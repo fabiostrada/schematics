31df6b6d248e779461cbb9ddbc5cd181681aee8d
Strada Fabio <fabio.strada@allitude.it>
2022-02-09 15:22:25 +0100

    update create-component schematic

--- src/create-component/files/__name@dasherize__/my__name@dasherize__.component.ts
+++ src/create-component/files/__name@dasherize__/my__name@dasherize__.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
-	selector: 'my-<%= dasherize(name) %>-component',
+	selector: '<%= dasherize(name) %>-component',
 	templateUrl: './<%= dasherize(name) %>.component.html',
 	styleUrls: ['./<%= dasherize(name) %>.component.scss'],
 })
-export class My<%= classify(name) %>Component implements OnInit {
+export class My<%= classify(name) %>Component implements OnInit, OnDestroy {
 	
     constructor() { }
 	
     ngOnInit(): void {
     }
     
+    ngOnDestroy(): void {
+        
+    }
 }
